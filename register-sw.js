8dabea18c9fff8909133d8208c7a30cdbd85a46a
radu456 <111914556+radu456@users.noreply.github.com>
2025-09-13 14:29:03 +0300

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,7 +1,7 @@
 // ĂncarcÄ SW dacÄ este suportat
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('/eMAG/sw.js')
       .catch(console.error);
   });
 }
